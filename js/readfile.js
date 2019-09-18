6310f9af06f986cc60650e1f600966f2ec85ccbb
Kim Nielsen (KINI) <KINI@ucsyd.dk>
2019-09-18 11:26:56 +0200

    0.3 CSS added, extern script read extren file with page content

--- js/readfile.js
+++ js/readfile.js
@@ -2,10 +2,8 @@
 $(document).ready(function () {
     $.get('../../risikovurderingtekstfiler/risikovurdering_forsidetekst.txt', function (data) {
         var lines = data.split("\n");
-        console.log("lines" + lines);
-
         $.each(lines, function (n, elem) {
-            $('#myContainer').append('<div>' + elem + '</div>');
+            $('#myContainer').append('<p>' + elem + '</p>');
         });
     });
 });
