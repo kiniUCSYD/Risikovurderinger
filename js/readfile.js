0d6505e1bf8807753f31d56714e012f26fab98db
Kim Nielsen (KINI) <KINI@ucsyd.dk>
2019-09-18 10:53:49 +0200

    demo 0.2 Extern script to read extern file

--- js/readfile.js
+++ js/readfile.js
@@ -1,11 +1,8 @@
 // JavaScript source code
 $(document).ready(function () {
-    console.log('in document ready');
-    $.get('../Risikovurderinger/risikovuderinger_forside.txt', function (data) {
+    $.get('../../risikovurderingtekstfiler/risikovurdering_forsidetekst.txt', function (data) {
         var lines = data.split("\n");
-
-        console.log('in get');
-        console.log('lines: ' + lines);
+        console.log("lines" + lines);
 
         $.each(lines, function (n, elem) {
             $('#myContainer').append('<div>' + elem + '</div>');
